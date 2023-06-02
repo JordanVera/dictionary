b546cf95ddbd32b9edbadd909550fc8bc9fa6b5e
Jordan Vera <verawebdevelopment@gmail.com>
2023-06-02 01:04:25 -0500

    definitionns component now loops through  all meanings

--- src/Components/Definitions.jsx
+++ src/Components/Definitions.jsx
@@ -5,28 +5,35 @@ const Definitions = ({ apiResponse }) => {
       <h1 className="h1">{apiResponse[0].word}</h1>
       <h2 className="phonetic">{apiResponse[0].phonetic}</h2>
       {/* <p>{apiResponse[0].meanings[0].definitions[0].definition}</p> */}
-      <h2 className="partOfSpeech">
-        <span>{apiResponse[0].meanings[0].partOfSpeech}</span>
-      </h2>
-      <h3 className="meaning">Meaning</h3>
-      <ul>
-        {apiResponse[0].meanings[0].definitions.map((meaning, i) => {
-          return (
-            <>
-              <li>
-                <span>{meaning.definition}</span>
-              </li>
-            </>
-          );
-        })}
-      </ul>
-      <h3 className="meaning">
-        Synonoyms:{' '}
-        <span className="purpleTxt syn">
-          {apiResponse[0].meanings[0].synonyms}
-        </span>
-      </h3>
-      <h3></h3>
+
+      {apiResponse[0].meanings.map((meaning, i) => {
+        return (
+          <>
+            <h2 className="partOfSpeech">
+              <span>{meaning.partOfSpeech}</span>
+            </h2>
+
+            <h3 className="meaning">Meaning</h3>
+            <ul>
+              {apiResponse[0].meanings[i].definitions.map((meaning, i) => {
+                return (
+                  <>
+                    <li>
+                      <span>{meaning.definition}</span>
+                    </li>
+                  </>
+                );
+              })}
+            </ul>
+            <h3 className="meaning">
+              Synonoyms:{' '}
+              <span className="purpleTxt syn">
+                {apiResponse[0].meanings[i].synonyms}
+              </span>
+            </h3>
+          </>
+        );
+      })}
     </div>
   );
 };
